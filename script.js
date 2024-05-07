874c1092f7d9ea742dfdce853ef450142a681af8
bee0511 <bee000092@gmail.com>
2024-05-07 17:44:46 +0800

    fix: scoring problem

--- script.js
+++ script.js
@@ -5,9 +5,19 @@ class Obstacle {
         this.moveInterval = null;
     }
 
+    getDimensions() {
+        const style = window.getComputedStyle(this.element, null);
+        return {
+            width: parseInt(style.getPropertyValue('width')),
+            height: parseInt(style.getPropertyValue('height')),
+            left: parseInt(style.getPropertyValue('left')),
+            top: parseInt(style.getPropertyValue('top')),
+        };
+    }
+
     startMoving() {
         this.moveInterval = setInterval(() => {
-            const ox = parseInt(window.getComputedStyle(this.element, null).getPropertyValue('left'));
+            const ox = this.getDimensions().left;
             if (ox + this.speed < 0) {
                 this.element.style.left = window.innerWidth + 'px';
             } else {
@@ -20,7 +30,6 @@ class Obstacle {
         clearInterval(this.moveInterval);
     }
 }
-
 class Dino {
     constructor() {
         this.element = document.querySelector('.dino');
@@ -31,16 +40,26 @@ class Dino {
         this.jumpSpeed = 20;
     }
 
+    getDimensions() {
+        const style = window.getComputedStyle(this.element, null);
+        return {
+            width: parseInt(style.getPropertyValue('width')),
+            height: parseInt(style.getPropertyValue('height')),
+            left: parseInt(style.getPropertyValue('left')),
+            top: parseInt(style.getPropertyValue('top')),
+        };
+    }
+
     jump() {
         if (this.isJumping) {
             return;
         }
         this.isJumping = true;
-        let start = parseInt(window.getComputedStyle(this.element, null).getPropertyValue('top'));
+        let start = this.getDimensions().top;
         let direction = -1;
         let originalStart = start;
         let jumpInterval = setInterval(() => {
-            let currentTop = parseInt(window.getComputedStyle(this.element, null).getPropertyValue('top'));
+            let currentTop = this.getDimensions().top;
             if (currentTop <= originalStart - this.maxJumpHeight && direction === -1) {
                 direction = 1;
             } else if (currentTop >= originalStart && direction === 1) {
@@ -58,16 +77,21 @@ class Dino {
     moveRight() {
         clearInterval(this.moveInterval);
         this.moveInterval = setInterval(() => {
-            const dx = parseInt(window.getComputedStyle(this.element, null).getPropertyValue('left'));
-            this.element.style.left = dx + this.speed + "px";
+            const dx = this.getDimensions().left;
+            const dinoWidth = this.getDimensions().width;
+            if (dx + dinoWidth < window.innerWidth) {
+                this.element.style.left = dx + this.speed + "px";
+            }
         }, 10);
     }
 
     moveLeft() {
         clearInterval(this.moveInterval);
         this.moveInterval = setInterval(() => {
-            const dx = parseInt(window.getComputedStyle(this.element, null).getPropertyValue('left'));
-            this.element.style.left = dx - this.speed + "px";
+            const dx = this.getDimensions().left;
+            if (dx > 0) {
+                this.element.style.left = dx - this.speed + "px";
+            }
         }, 10);
     }
 
@@ -86,43 +110,57 @@ class Game {
         this.obstacle = new Obstacle(5); // Create a new Obstacle instance
         this.gameOver = document.querySelector('.gameOver');
         this.scoreCont = document.getElementById('scoreCont');
+        this.lastRun = 0;
+        this.delay = 10; // delay in milliseconds
     }
 
     start() {
         // setTimeout(() => {
         //     this.audiogo.play();
         // }, 500);
-        this.handleKeydown();
-        this.handleKeyup();
+        this.handleKey();
         this.checkCollision();
         this.obstacle.startMoving(); // Start moving the obstacle
     }
 
-    handleKeydown() {
-        document.onkeydown = (e) => {
-            if(this.isGameOver) {
-                return;
-            }
-            if (e.key == 'ArrowUp') {
-                this.dino.jump();
-            }
-            if (e.key == 'ArrowRight') {
-                this.dino.moveRight();
-            }
-            if (e.key == 'ArrowLeft') {
-                this.dino.moveLeft();
-            }
+    handleKey() {
+        document.onkeydown = document.onkeyup = (e) => {
+            this.processKeyEvent(e);
         }
     }
 
-    handleKeyup() {
-        document.onkeyup = (e) => {
-            if (this.isGameOver) { 
-                return;
-            }
-            if (e.key == 'ArrowRight' || e.key == 'ArrowLeft') {
-                this.dino.stopMoving();
-            }
+    processKeyEvent(e) {
+        if (this.isGameOver) {
+            return;
+        }
+        if (e.type == 'keydown') {
+            this.processKeyDownEvent(e.key);
+        } else if (e.type == 'keyup') {
+            this.processKeyUpEvent(e.key);
+        }
+    }
+
+    processKeyDownEvent(key) {
+        const now = Date.now();
+        if ((key == 'ArrowRight' || key == 'ArrowLeft') && now - this.lastRun < this.delay) {
+            return;
+        }
+        this.lastRun = now;
+
+        if (key == 'ArrowUp') {
+            this.dino.jump();
+        }
+        if (key == 'ArrowRight') {
+            this.dino.moveRight();
+        }
+        if (key == 'ArrowLeft') {
+            this.dino.moveLeft();
+        }
+    }
+
+    processKeyUpEvent(key) {
+        if (key == 'ArrowRight' || key == 'ArrowLeft') {
+            this.dino.stopMoving();
         }
     }
 
@@ -137,33 +175,39 @@ class Game {
         let lastScoreUpdate = Date.now();
 
         setInterval(() => {
-            const dinoStyle = window.getComputedStyle(this.dino.element, null);
-            const obstacleStyle = window.getComputedStyle(this.obstacle.element, null);
+            const dinoDimensions = this.dino.getDimensions();
+            const obstacleDimensions = this.obstacle.getDimensions();
 
-            const dinoWidth = parseInt(dinoStyle.getPropertyValue('width'));
-            const dinoHeight = parseInt(dinoStyle.getPropertyValue('height'));
-            const obstacleWidth = parseInt(obstacleStyle.getPropertyValue('width'));
-            const obstacleHeight = parseInt(obstacleStyle.getPropertyValue('height'));
-
-            const dx = parseInt(dinoStyle.getPropertyValue('left'));
-            const dy = parseInt(dinoStyle.getPropertyValue('top'));
-
-            const ox = parseInt(obstacleStyle.getPropertyValue('left'));
-            const oy = parseInt(obstacleStyle.getPropertyValue('top'));
-
-            if (dx < ox + obstacleWidth &&
-                dx + dinoWidth > ox &&
-                dy < oy + obstacleHeight &&
-                dy + dinoHeight > oy) {
+            if (this.isColliding(dinoDimensions, obstacleDimensions)) {
                 this.endGame();
                 return; 
             }
 
-            if (dx > ox && dy < oy && Date.now() - lastScoreUpdate >= 1000) {
+            if (this.isScoring(dinoDimensions, obstacleDimensions) && Date.now() - lastScoreUpdate >= 1000) {
                 this.updateScore();
                 lastScoreUpdate = Date.now();
             }
-        }, 100);
+
+        }, this.delay);
+    }
+
+    isColliding(dinoDimensions, obstacleDimensions) {
+        return dinoDimensions.left < obstacleDimensions.left + obstacleDimensions.width &&
+            dinoDimensions.left + dinoDimensions.width > obstacleDimensions.left &&
+            dinoDimensions.top < obstacleDimensions.top + obstacleDimensions.height &&
+            dinoDimensions.top + dinoDimensions.height > obstacleDimensions.top;
+    }
+
+    isScoring(dinoDimensions, obstacleDimensions) {
+        const dinoBottom = dinoDimensions.top + dinoDimensions.height; 
+        const dinoRight = dinoDimensions.left + dinoDimensions.width;
+        const obstacleTop = obstacleDimensions.top;
+        const obstacleRight = obstacleDimensions.left + obstacleDimensions.width;
+        const offset = 10;
+
+        return dinoBottom <= obstacleTop && 
+            dinoDimensions.left > obstacleDimensions.left && 
+            dinoRight < obstacleRight + offset;
     }
 
     updateScore() {
